0df4c18713612cb246214cb2cf8f2c23cf9f7572
egusev <egusev@snm-code.ru>
2022-10-25 17:49:48 +0300

    Теперь это кликер

--- app/src/main/java/com/egitoo/etesttool/MainActivity.java
+++ app/src/main/java/com/egitoo/etesttool/MainActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.renderscript.RenderScript;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,59 +20,42 @@ import com.egitoo.etesttool.databinding.ActivityMainBinding;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements NfcAdapter.ReaderCallback {
-    private NfcAdapter nfcAdapter;
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-    private String resp;
+public class MainActivity extends AppCompatActivity {
 
     public TextView textView;
-    Handler handler = new Handler();
+    public Button plus_But;
+    public Button min_But;
+    Integer count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         textView = findViewById(R.id.textView);
-        textView.setText("Wait....");
+        updateText(String.format(Locale.ENGLISH,"Click: %d", count));
+        min_But = (Button) findViewById(R.id.minus_button);
+        plus_But = (Button) findViewById(R.id.plus_button);
 
-    }
-
-    private void updateTextView(final String s) {
-        MainActivity.this.runOnUiThread(new Runnable() {
+        plus_But.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                TextView tv = (TextView) findViewById(R.id.textView);
-                tv.setText(s);
+            public void onClick(View v) {
+                count = count + 1;
+                updateText(String.format(Locale.ENGLISH,"Click: %d", count));
+            }
+        });
+
+        min_But.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = count - 1;
+                updateText(String.format(Locale.ENGLISH,"Click: %d", count));
             }
         });
     }
 
-    @Override public void onResume() {
-        super.onResume();
-        if(nfcAdapter != null) {
-            nfcAdapter.enableReaderMode(this, this,
-                    NfcAdapter.FLAG_READER_NFC_A,
-                    null);
-        }
-    }
-
-    @Override public void onPause() {
-        super.onPause();
-        nfcAdapter.disableReaderMode(this);
-    }
-
-    @Override
-    public void onTagDiscovered(Tag tag) {
-        IsoDep isoDep = IsoDep.get(tag);
-        try {
-            resp = UtilsJa.bytesToHex(isoDep.transceive(UtilsJa.hexStringToByteArray("00A4040007A0000002471001")));
-            Log.println(Log.DEBUG, "onTagDiscovered", resp);
-            updateTextView(resp);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void updateText (String text){
+        textView.setText(text);
     }
 }
